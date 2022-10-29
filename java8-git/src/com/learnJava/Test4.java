ce09dc3817fe2410c22ba71639cfb626ce8d0ced
Soumyajeet Patra <ipunsoumya@gmail.com>
2022-10-29 13:04:55 +0530

    First Commit

---
+++ java8-git/src/com/learnJava/Test4.java
@@ -0,0 +1,21 @@
+package com.learnJava;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+
+	
+	for (int i = -1; i>=-10; i--) {
+		System.out.println(i);
+	}
+	
+	}
+	
+	 void getName() {
+		 
+	 }
+	 
+void getName() {
+		 
+	 }
+}
